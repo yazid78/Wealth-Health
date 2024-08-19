2767d963e24bb586d3bf48608e6ef8adad934fb3
Yazid <yazid780@outlook.fr>
2024-08-19 18:41:14 +0200

    add gestion d'erreur et readme

---
+++ Project React/src/components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+
+
+function ErrorPage() {
+    return (
+        <div>
+            <h1>Oops! Something went wrong.</h1>
+            <p>The page you're looking for does not exist or another error occurred.</p>
+        </div>
+    );
+}
+
+export default ErrorPage;
--- Project React/src/main.tsx
+++ Project React/src/main.tsx
@@ -5,17 +5,21 @@ import store from './store.ts'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home.tsx'
 import ListEmployee from './components/Employee.tsx'
+import ErrorPage from './components/ErrorPage.tsx'
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
 
   },
   {
     path: "/employee",
     element: <ListEmployee />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
